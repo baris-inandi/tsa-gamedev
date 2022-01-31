6f9b7263e4c6b5d3a1cfe526de899590f3638e4a
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-01-31 17:33:38 +0300

    commit

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RiptideNetworking;
 
 public class Player : MonoBehaviour
 {
-    public ushort id { get; set; }
+	public ushort id { get; set; }
+	public ushort hp { get; set; }
 
+	public void SendHP()
+	{
+		Message msg = Message.Create(MessageSendMode.reliable, Packets.ClientToHostId.SendHP);
+	}
 }
